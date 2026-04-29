b73f0db17c0486033c24da59ecbba31e918f770b
Padala-Varun <varun2004varun2004@gmail.com>
2026-04-29 16:26:58 +0530

    Add solutions for Leetcode 455 Assign Cookies and 2 Fractional Knapsack problems

---
+++ Greedy-Problems/2fractional-knapsack.py
@@ -0,0 +1,13 @@
+def fractionalKnapsack(W, arr, n):
+    arr.sort(key = lambda x: x.value/x.weight, reverse=True)
+    curWeight = 0
+    finalValue = 0
+    for i in range(n):
+        if curWeight + arr[i].weight <= W:
+            curWeight += arr[i].weight
+            finalValue += arr[i].value
+        else:
+            remain = W - curWeight
+            finalValue += arr[i].value/arr[i].weight * remain
+            break
+    return finalValue
