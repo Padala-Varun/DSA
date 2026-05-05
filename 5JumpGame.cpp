16455a21b3d7eefac4cc43c377e85ee62dbdf9ab
Padala-Varun <varun2004varun2004@gmail.com>
2026-05-05 13:41:33 +0530

    Add solution for LeetCode 55 - Jump Game problem

---
+++ 5JumpGame.cpp
@@ -0,0 +1,16 @@
+//Leetcode - 55 Jump Game - Greedy Approach
+
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+       int max_index=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(i>max_index)
+            return false;
+            max_index=max(max_index,i+nums[i]);
+        }
+        return true;
+        
+    }
+};
